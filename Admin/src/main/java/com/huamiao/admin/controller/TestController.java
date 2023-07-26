e905eed24f32658f1c672d8f1862b32cbb76e7dc
朱振华 <zhuzhenhua@benlai.com>
2023-07-26 11:15:36 +0800

    增加测试接口

---
+++ Admin/src/main/java/com/huamiao/admin/controller/TestController.java
@@ -0,0 +1,35 @@
+package com.huamiao.admin.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import com.huamiao.common.base.UserSession;
+import com.huamiao.common.entity.ResponseVo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈测试控制器〉
+ *
+ * @author ZZH
+ * @create 2023/7/25
+ * @since 1.0.0
+ */
+@RestController
+@RequestMapping("test")
+@Api("测试接口")
+public class TestController {
+
+    @PostMapping("token")
+    @ApiOperation("测试request")
+    public ResponseVo<Object> testToken(HttpServletRequest request){
+        Object authorization = request.getAttribute("Authorization");
+        System.out.println(authorization.toString());
+        return ResponseVo.success(JSONObject.toJSONString(UserSession.getUser()));
+    }
+
+}
